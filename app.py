904d4dec803d304e249860b3c42f197ff2dc1e2f
Jason <xiongjason2002@gmail.com>
2021-01-17 00:47:17 -0500

    finishing frontend, switch to pushshift

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from reddit_data import get_from_reddit, get_graph_data
 
 app = Flask(__name__)
 app.config.from_pyfile("config.py")
@@ -7,5 +8,17 @@ app.config.from_pyfile("config.py")
 def index():
     return render_template('index.html')
 
+@app.route('/search', methods = ['POST', 'GET'])
+def success():
+    if request.method == 'POST':
+        query = request.form['query'].lower()
+        data = get_from_reddit(query)
+        graphdata = get_graph_data(query)
+        return render_template("index.html", data=data)
+        if data:
+            return render_template("index.html", data=data)
+    
+    return render_template("index.html")
+        
 if __name__ == '__main__':
     app.run()
--- reddit_data.py
+++ reddit_data.py
@@ -1,11 +1,90 @@
 import praw
 from config import client_id, client_secret
 import re
+from textblob import TextBlob
+import datetime as dt
+from psaw import PushshiftAPI
+
 
 reddit = praw.Reddit(
     client_id=client_id,
     client_secret=client_secret,
     user_agent="review finder"
 )
-for submission in reddit.subreddit("learnpython").hot(limit=10):
-    print(submission.title)
+api = PushshiftAPI(reddit)
+
+def get_from_reddit(item):
+
+    results = {}
+    reddit_posts = reddit.subreddit("all").search(
+        item, 
+        sort="best", 
+        time_filter="week", 
+        limit = 10,
+    )
+
+    for item in reddit_posts:
+        #print(item.title)
+
+        results[item.title] = {
+            "text": item.selftext,
+            "url": "https://www.reddit.com" + item.permalink,
+        }
+        #print(item.permalink)
+    return results
+
+def searchAPI(start_epoch, end_epoch, item):
+    gen =api.search_submissions(before=end_epoch,
+                    after=start_epoch,
+                    q = item,
+                    sort = "desc",
+                    sort_type = "score",
+                    limit=15,
+                    title=item,
+                    over_18="false"
+                    )
+    print("bruh1")
+    cur = 0
+    for item in gen:
+        print(item.permalink)
+        sent = TextBlob(item.title).sentiment
+        cur+=sent.polarity
+        print(sent)
+    return cur
+
+
+def get_graph_data(item):
+    results = [[0 for x in range(2)] for y in range(15)] 
+    start_epoch=int(dt.datetime(2020, 1, 1).timestamp())
+    for i in range(1, 13):
+        results[i][0] = 0
+    for i in range(9, 14):
+        api = PushshiftAPI(reddit)
+        print(i)
+
+        if i == 12:
+            start_epoch=int(dt.datetime(2020, i, 1).timestamp())
+            end_epoch=int(dt.datetime(2021, 1, 1).timestamp())
+        elif i == 13:
+            start_epoch=int(dt.datetime(2021, 1, 1).timestamp())
+            end_epoch=int(dt.datetime(2021, 2, 1).timestamp())
+        else:
+            start_epoch=int(dt.datetime(2020, i, 1).timestamp())
+            end_epoch=int(dt.datetime(2020, i+1, 1).timestamp())
+        
+        results[i][0] = searchAPI(start_epoch, end_epoch, item)
+        print("results: ", results[i][0])
+        
+
+
+
+
+
+    return results
+
+
+#for submission in reddit.subreddit("mechanicalkeyboards").hot(limit=10):
+ #   testimonial = TextBlob(submission.title)
+  #  print(submission.title)
+   # print(testimonial.sentiment)
+
